9fa4f5516cd0b4ad40ba001b4acf8e01b087daab
Thomas Gossart <tg.59290@gmail.com>
2017-06-15 11:39:34 +0200

    added comments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+// Get the input to display the result
 var result = document.getElementById("result");
 
+// On click on number or operator adds it to the result input
 function getValue(object) {
   result.value += object.innerHTML;
 }
 
+// Clear the result input
 function clearAll() {
   result.value = "";
 }
 
+// On click on = eval the content of the result input content and display the result
 function operate() {
   var operation = result.value;
   var finalResult = eval(operation);
