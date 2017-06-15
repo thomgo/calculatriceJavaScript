b1cf325ee8c73761e986089ae8c6e8103912f474
Thomas Gossart <tg.59290@gmail.com>
2017-06-15 11:34:54 +0200

    added basic code to make the maths

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+var result = document.getElementById("result");
 
+function getValue(object) {
+  result.value += object.innerHTML;
+}
+
+function clearAll() {
+  result.value = "";
+}
+
+function operate() {
+  var operation = result.value;
+  var finalResult = eval(operation);
+  result.value = finalResult;
+}
