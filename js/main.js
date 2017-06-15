90aaa09be698a5d58d25b65ab2ad59129ee579ae
Thomas Gossart <tg.59290@gmail.com>
2017-06-15 13:51:07 +0200

    calc works as the apple one with full number

--- js/main.js
+++ js/main.js
@@ -22,23 +22,39 @@
 // Get the input to display the result
 var result = document.getElementById("result");
 var currentOperation = "";
+var triggerOperation = false;
 
 // On click on number or operator adds it to the result input
 function getValue(object) {
+  if (triggerOperation === true) {
+    result.value = "";
+  }
   currentOperation += object.innerHTML;
-  result.value = object.innerHTML;
+  result.value += object.innerHTML;
+  triggerOperation = false;
 }
 
 function getOperator(object) {
+  if (result.value !== ""){
+    currentOperation = result.value;
+  }
   currentOperation += object.innerHTML;
+  triggerOperation = true;
 }
 // Clear the result input
 function clearAll() {
   result.value = "";
+  currentOperation = "";
 }
 
 // On click on = eval the content of the result input content and display the result
 function operate() {
-  result.value = eval(currentOperation);
-  currentOperation = eval(currentOperation);
+  var finalResult = eval(currentOperation);
+  if(isNaN(finalResult)) {
+    result.value  = "Error";
+  }
+  else {
+    result.value = finalResult;
+    currentOperation = finalResult;
+  }
 }
