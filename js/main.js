35546df0625e8302e9c50d0d4f8118b89d7f4b4e
Thomas Gossart <tg.59290@gmail.com>
2017-06-15 11:57:40 +0200

    made it look like the apple one, the entire operation is not written on screen

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
+// // Get the input to display the result
+// var result = document.getElementById("result");
+//
+// // On click on number or operator adds it to the result input
+// function getValue(object) {
+//   result.value += object.innerHTML;
+// }
+//
+// // Clear the result input
+// function clearAll() {
+//   result.value = "";
+// }
+//
+// // On click on = eval the content of the result input content and display the result
+// function operate() {
+//   var operation = result.value;
+//   var finalResult = eval(operation);
+//   result.value = finalResult;
+// }
+
+
 // Get the input to display the result
 var result = document.getElementById("result");
+var currentOperation = "";
 
 // On click on number or operator adds it to the result input
 function getValue(object) {
-  result.value += object.innerHTML;
+  currentOperation += object.innerHTML;
+  result.value = object.innerHTML;
 }
 
+function getOperator(object) {
+  currentOperation += object.innerHTML;
+}
 // Clear the result input
 function clearAll() {
   result.value = "";
@@ -13,7 +39,6 @@ function clearAll() {
 
 // On click on = eval the content of the result input content and display the result
 function operate() {
-  var operation = result.value;
-  var finalResult = eval(operation);
-  result.value = finalResult;
+  result.value = eval(currentOperation);
+  currentOperation = eval(currentOperation);
 }
